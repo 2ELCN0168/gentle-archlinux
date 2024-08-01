2e9a227ede0f299810a3f6b1fd5b59ce88cf3b29
MH3PZHARXXX001O <157519230+2ELCN0168@users.noreply.github.com>
2024-08-01 14:47:58 +0200

    Modified files

--- functions/f_cpu_manufacturer.sh
+++ functions/f_cpu_manufacturer.sh
@@ -1,30 +1,28 @@
 # FUNCTION(S)
 # ---
 # This function aims to get the CPU manufacturer to install the apropriate microcode in a later stage.
+# EDIT 1 : Modified variables declarations and tests in conditions + replaced by echo.
 # ---
 
 get_cpu_brand() {
 
-  local vendor
-  export cpuBrand=""
+        declare vendor=""
+        declare -gx cpuBrand=""
 
-  vendor=$(lscpu | grep -i vendor | awk '{ print $3 }' | head -1)
+        vendor="$(lscpu | grep -i vendor | awk '{ print $3 }' | head -1)"
 
-  case "$vendor" in
-    "GenuineIntel")
-      printf "${C_WHITE}> ${INFO} ${C_CYAN}INTEL CPU${NO_FORMAT} detected."
-      jump
-      cpuBrand="INTEL"
-      ;;
-    "AuthenticAMD")
-      printf "${C_WHITE}> ${INFO} ${C_RED}AMD CPU${NO_FORMAT} detected."
-      jump
-      cpuBrand="AMD"
-      ;;
-    *)  
-      printf "${C_WHITE}> ${INFO} ${C_YELLOW}Could not detect your CPU vendor. No microcode will be installed.${NO_FORMAT}"
-      jump
-      cpuBrand="UNKNOWN"
-      ;;
-  esac
+        case "${vendor}" in
+                "GenuineIntel")
+                        echo -e "${C_WHITE}> ${INFO} ${C_CYAN}INTEL CPU${NO_FORMAT} detected.\n"
+                        cpuBrand="INTEL"
+                        ;;
+                "AuthenticAMD")
+                        echo -e "${C_WHITE}> ${INFO} ${C_RED}AMD CPU${NO_FORMAT} detected.\n"
+                        cpuBrand="AMD"
+                        ;;
+                *)  
+                        echo -e "${C_WHITE}> ${INFO} ${C_YELLOW}Could not detect your CPU vendor. No microcode will be installed.${NO_FORMAT}\n"
+                        cpuBrand="UNKNOWN"
+                        ;;
+        esac
 }
