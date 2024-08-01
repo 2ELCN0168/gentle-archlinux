f70b50ddbd036716abfef1fc49f74c499f0affc2
MH3PZHARXXX001O <157519230+2ELCN0168@users.noreply.github.com>
2024-08-01 15:20:17 +0200

    Modified files

--- functions/f_filesystem.sh
+++ functions/f_filesystem.sh
@@ -6,90 +6,67 @@
 
 filesystem_choice() {
 
-  export filesystem=""
+        declare -gx filesystem=""
 
-  while true; do
-    printf "==FILESYSTEM========"
-    jump
-    printf "${C_WHITE}[0] - ${C_YELLOW}BTRFS${NO_FORMAT} (default) \n"
-    printf "${C_WHITE}[1] - ${C_CYAN}XFS${NO_FORMAT} \n"
-    printf "${C_WHITE}[2] - ${C_RED}EXT4${NO_FORMAT} "
-    jump
-    printf "====================\n"
-    
-    read -p "Which filesystem do you want to use? [0/1/2] -> " response
-    response=${response:-0} # Default is BTRFS if no response given
-    jump
-    case "$response" in
-      [0])
-        filesystem="BTRFS"
-        printf "${C_WHITE}> ${INFO} ${NO_FORMAT}You chose ${C_WHITE}${filesystem}${NO_FORMAT}"
-        jump
-        break
-        ;;
-      [1])
-        filesystem="XFS"
-        printf "${C_WHITE}> ${INFO} ${NO_FORMAT}You chose ${C_WHITE}${filesystem}${NO_FORMAT}"
-        jump
-        break
-        ;;
-      [2])
-        filesystem="EXT4"
-        printf "${C_WHITE}> ${INFO} ${NO_FORMAT}You chose ${C_WHITE}${filesystem}${NO_FORMAT}"
-        jump
-        break
-        ;;
-      *)
-        invalid_answer
-        ;;
-    esac
-  done
+        while true; do
+                echo -e "==FILESYSTEM========\n"
+
+                echo -e "${C_WHITE}[0] - ${C_YELLOW}BTRFS${NO_FORMAT} (default)"
+                echo -e "${C_WHITE}[1] - ${C_CYAN}XFS${NO_FORMAT}"
+                echo -e "${C_WHITE}[2] - ${C_RED}EXT4${NO_FORMAT}"
+                
+                echo -e "====================\n"
+
+                echo -e "${B_CYAN} Which filesystemd do you want to use? [0/1/2] -> ${NO_FORMAT} \c"
+                
+                declare -i ans_filesystem="0"
+                read ans_filesystem
+                echo ""
+
+                case "${ans_filesystem}" in
+                        [0])
+                                filesystem="BTRFS"
+                                echo -e "${C_WHITE}> ${INFO} ${NO_FORMAT}You chose ${C_WHITE}${filesystem}${NO_FORMAT}\n"
+                                break
+                                ;;
+                        [1])
+                                filesystem="XFS"
+                                echo -e "${C_WHITE}> ${INFO} ${NO_FORMAT}You chose ${C_WHITE}${filesystem}${NO_FORMAT}\n"
+                                break
+                                ;;
+                        [2])
+                                filesystem="EXT4"
+                                echo -e "${C_WHITE}> ${INFO} ${NO_FORMAT}You chose ${C_WHITE}${filesystem}${NO_FORMAT}\n"
+                                break
+                                ;;
+                        *)
+                                invalid_answer
+                                ;;
+                esac
+        done
 }
 
 btrfs_handling() {
 
-  # FORMATTING DONE
+        # FORMATTING DONE
 
-  export btrfsSubvols=0
-  local btrfsQuotas=
+        export btrfsSubvols=0
+        local btrfsQuotas=
 
-  while true; do
-    read -p "[?] - It seems that you've picked BTRFS, do you want a clean installation with subvolumes (0) or a regular one with only the filesystem (1)? [0=default/1] -> " response
-    local response=${response:-0}
-    jump
-    case "$response" in
-      [0])
-        btrfsSubvols=1
-        printf "${C_WHITE}> ${INFO} ${C_GREEN}You chose to make subvolumes. Good choice.${NO_FORMAT}"
-        jump
-        break
-        ;;
-      [1])
-        btrfsSubvols=0
-        printf "${C_WHITE}> ${INFO} ${C_YELLOW}No subvolume will be created.${NO_FORMAT}"
-        jump
-        break
-        ;;
-      *)
-        invalid_answer
-        ;;
-    esac
-  done
-
-  if [ $btrfsSubvols -eq 1 ]; then
-          while true; do
-                read -p "[?] - Do you want to enable quotas on your subvolumes? [Y/n] " response
-                local response=${response:-Y}
+        while true; do
+                read -p "[?] - It seems that you've picked BTRFS, do you want a clean installation with subvolumes (0) or a regular one with only the filesystem (1)? [0=default/1] -> " response
+                local response=${response:-0}
+                jump
                 case "$response" in
-                        [yY])
-                                btrfsQuotas=1
-                                printf "${C_WHITE}> ${INFO} ${C_GREEN}You chose to enable quotas.${NO_FORMAT}"
+                        [0])
+                                btrfsSubvols=1
+                                echo -e "${C_WHITE}> ${INFO} ${C_GREEN}You chose to make subvolumes. Good choice.${NO_FORMAT}"
                                 jump
                                 break
                                 ;;
-                        [nN])
-                                btrfsQuotas=0
-                                printf "${C_WHITE}> ${INFO} ${C_YELLOW}There will be no quotas on your subvolumes.${NO_FORMAT}"
+                        [1])
+                                btrfsSubvols=0
+                                echo -e "${C_WHITE}> ${INFO} ${C_YELLOW}No subvolume will be created.${NO_FORMAT}"
                                 jump
                                 break
                                 ;;
@@ -97,90 +74,113 @@ btrfs_handling() {
                                 invalid_answer
                                 ;;
                 esac
-          done
-  fi
+        done
 
-  printf "${C_WHITE}> ${INFO} Formatting ${root_part} to ${filesystem}.${NO_FORMAT}\n"
-  mkfs.btrfs -f -L Archlinux ${root_part} &> /dev/null
+        if [ $btrfsSubvols -eq 1 ]; then
+                while true; do
+                        read -p "[?] - Do you want to enable quotas on your subvolumes? [Y/n] " response
+                        local response=${response:-Y}
+                        case "$response" in
+                                [yY])
+                                btrfsQuotas=1
+                                echo -e "${C_WHITE}> ${INFO} ${C_GREEN}You chose to enable quotas.${NO_FORMAT}"
+                                jump
+                                break
+                                ;;
+                        [nN])
+                                btrfsQuotas=0
+                                echo -e "${C_WHITE}> ${INFO} ${C_YELLOW}There will be no quotas on your subvolumes.${NO_FORMAT}"
+                                jump
+                                break
+                                ;;
+                        *)
+                                invalid_answer
+                                ;;
+                        esac
+                done
+        fi
 
-  if [[ $btrfsSubvols -eq 1 ]]; then
-    mount ${root_part} /mnt &> /dev/null
-    btrfs subvolume create /mnt/{@,@home,@usr,@tmp,@var,.snapshots} &> /dev/null
+        echo -e "${C_WHITE}> ${INFO} Formatting ${root_part} to ${filesystem}.${NO_FORMAT}\n"
+        mkfs.btrfs -f -L Archlinux ${root_part} &> /dev/null
 
-    printf "${C_WHITE}> ${INFO} Creating${NO_FORMAT} ${C_YELLOW}subvolume ${C_GREEN}@${NO_FORMAT}\n"
-    printf "${C_WHITE}> ${INFO} Creating${NO_FORMAT} ${C_YELLOW}subvolume ${C_GREEN}@home${NO_FORMAT}\n"
-    printf "${C_WHITE}> ${INFO} Creating${NO_FORMAT} ${C_YELLOW}subvolume ${C_GREEN}@usr${NO_FORMAT}\n"
-    printf "${C_WHITE}> ${INFO} Creating${NO_FORMAT} ${C_YELLOW}subvolume ${C_GREEN}@tmp${NO_FORMAT}\n"
-    printf "${C_WHITE}> ${INFO} Creating${NO_FORMAT} ${C_YELLOW}subvolume ${C_GREEN}@var${NO_FORMAT}\n"
-    printf "${C_WHITE}> ${INFO} Creating${NO_FORMAT} ${C_YELLOW}subvolume ${C_GREEN}.snapshots${NO_FORMAT}"
-    jump
+        if [[ $btrfsSubvols -eq 1 ]]; then
+                mount ${root_part} /mnt &> /dev/null
+                btrfs subvolume create /mnt/{@,@home,@usr,@tmp,@var,.snapshots} &> /dev/null
 
-    umount -R /mnt &> /dev/null
+                echo -e "${C_WHITE}> ${INFO} Creating${NO_FORMAT} ${C_YELLOW}subvolume ${C_GREEN}@${NO_FORMAT}\n"
+                echo -e "${C_WHITE}> ${INFO} Creating${NO_FORMAT} ${C_YELLOW}subvolume ${C_GREEN}@home${NO_FORMAT}\n"
+                echo -e "${C_WHITE}> ${INFO} Creating${NO_FORMAT} ${C_YELLOW}subvolume ${C_GREEN}@usr${NO_FORMAT}\n"
+                echo -e "${C_WHITE}> ${INFO} Creating${NO_FORMAT} ${C_YELLOW}subvolume ${C_GREEN}@tmp${NO_FORMAT}\n"
+                echo -e "${C_WHITE}> ${INFO} Creating${NO_FORMAT} ${C_YELLOW}subvolume ${C_GREEN}@var${NO_FORMAT}\n"
+                echo -e "${C_WHITE}> ${INFO} Creating${NO_FORMAT} ${C_YELLOW}subvolume ${C_GREEN}.snapshots${NO_FORMAT}"
+                jump
 
-    printf "${C_WHITE}> ${INFO} Mounting ${C_GREEN}@${NO_FORMAT} to ${C_WHITE}/mnt${NO_FORMAT}\n"
-    mount -t btrfs -o compress=zstd,discard=async,autodefrag,subvol=@ ${root_part} /mnt
+                umount -R /mnt &> /dev/null
 
-    printf "${C_WHITE}> ${INFO} Mounting ${C_GREEN}@home${NO_FORMAT} to ${C_WHITE}/mnt/home${NO_FORMAT}\n"
-    mount --mkdir -t btrfs -o compress=zstd,discard=async,autodefrag,subvol=@home ${root_part} /mnt/home
+                echo -e "${C_WHITE}> ${INFO} Mounting ${C_GREEN}@${NO_FORMAT} to ${C_WHITE}/mnt${NO_FORMAT}\n"
+                mount -t btrfs -o compress=zstd,discard=async,autodefrag,subvol=@ ${root_part} /mnt
 
-    printf "${C_WHITE}> ${INFO} Mounting ${C_GREEN}@usr${NO_FORMAT} to ${C_WHITE}/mnt/usr${NO_FORMAT}\n"
-    mount --mkdir -t btrfs -o compress=zstd,discard=async,autodefrag,subvol=@usr ${root_part} /mnt/usr
+                echo -e "${C_WHITE}> ${INFO} Mounting ${C_GREEN}@home${NO_FORMAT} to ${C_WHITE}/mnt/home${NO_FORMAT}\n"
+                mount --mkdir -t btrfs -o compress=zstd,discard=async,autodefrag,subvol=@home ${root_part} /mnt/home
 
-    printf "${C_WHITE}> ${INFO} Mounting ${C_GREEN}@tmp${NO_FORMAT} to ${C_WHITE}/mnt/tmp${NO_FORMAT}\n"
-    mount --mkdir -t btrfs -o compress=zstd,discard=async,autodefrag,subvol=@tmp ${root_part} /mnt/tmp
+                echo -e "${C_WHITE}> ${INFO} Mounting ${C_GREEN}@usr${NO_FORMAT} to ${C_WHITE}/mnt/usr${NO_FORMAT}\n"
+                mount --mkdir -t btrfs -o compress=zstd,discard=async,autodefrag,subvol=@usr ${root_part} /mnt/usr
 
-    printf "${C_WHITE}> ${INFO} Mounting ${C_GREEN}@var${NO_FORMAT} to ${C_WHITE}/mnt/var${NO_FORMAT}\n"
-    mount --mkdir -t btrfs -o compress=zstd,discard=async,autodefrag,subvol=@var ${root_part} /mnt/var
+                echo -e "${C_WHITE}> ${INFO} Mounting ${C_GREEN}@tmp${NO_FORMAT} to ${C_WHITE}/mnt/tmp${NO_FORMAT}\n"
+                mount --mkdir -t btrfs -o compress=zstd,discard=async,autodefrag,subvol=@tmp ${root_part} /mnt/tmp
 
-    printf "${C_WHITE}> ${INFO} Mounting ${C_GREEN}.snapshots${NO_FORMAT} to ${C_WHITE}/mnt/.snapshots${NO_FORMAT}\n"
-    mount --mkdir -t btrfs -o compress=zstd,discard=async,autodefrag,subvol=.snapshots ${root_part} /mnt/.snapshots
+                echo -e "${C_WHITE}> ${INFO} Mounting ${C_GREEN}@var${NO_FORMAT} to ${C_WHITE}/mnt/var${NO_FORMAT}\n"
+                mount --mkdir -t btrfs -o compress=zstd,discard=async,autodefrag,subvol=@var ${root_part} /mnt/var
 
-    printf "${C_WHITE}> ${INFO} Mounting ${C_GREEN}/dev/sda1${NO_FORMAT} to ${C_WHITE}/mnt/boot${NO_FORMAT}\n"
-    mount --mkdir ${boot_part} /mnt/boot
-    jump
+                echo -e "${C_WHITE}> ${INFO} Mounting ${C_GREEN}.snapshots${NO_FORMAT} to ${C_WHITE}/mnt/.snapshots${NO_FORMAT}\n"
+                mount --mkdir -t btrfs -o compress=zstd,discard=async,autodefrag,subvol=.snapshots ${root_part} /mnt/.snapshots
 
-    lsblk -f
-  elif [[ $btrfsSubvols -eq 0 ]]; then
-    mount_default
-  fi
+                echo -e "${C_WHITE}> ${INFO} Mounting ${C_GREEN}/dev/sda1${NO_FORMAT} to ${C_WHITE}/mnt/boot${NO_FORMAT}\n"
+                mount --mkdir ${boot_part} /mnt/boot
+                jump
+
+                lsblk -f
+        elif [[ $btrfsSubvols -eq 0 ]]; then
+                mount_default
+        fi
 
 
-  if [[ $btrfsSubvols -eq 1 && $btrfsQuotas -eq 1 ]]; then
-        
-        # MUST BE REFORMATTED
+        if [[ $btrfsSubvols -eq 1 && $btrfsQuotas -eq 1 ]]; then
 
-        btrfs quota enable /mnt/var      
-        btrfs quota enable /mnt/tmp      
-        btrfs quota rescan /mnt/var
-        btrfs quota rescan /mnt/tmp
-        btrfs qgroup limit 2G /mnt/tmp
-        btrfs qgroup limit 5G /mnt/var
-  fi
+                # MUST BE REFORMATTED
+
+                btrfs quota enable /mnt/var      
+                btrfs quota enable /mnt/tmp      
+                btrfs quota rescan /mnt/var
+                btrfs quota rescan /mnt/tmp
+                btrfs qgroup limit 2G /mnt/tmp
+                btrfs qgroup limit 5G /mnt/var
+        fi
 }
-  
+
 fs_handling() {
 
-  export LVM=0
+        export LVM=0
 
-  # FORMATTING DONE
-  while true; do
-    read -p "It seems that you've picked ${filesystem}, do you want to use LVM? [Y/n] -> " response
-    response=${response:-Y}
-    case "$response" in
-      [yY])
-      LVM=1
-      break
-      ;;            
-    [nN])
-      LVM=0
-      printf "${C_WHITE}> ${NO_FORMAT}You won't use LVM."
-      jump
-      break
-      ;;
-    *)
-      invalid_answer
-      ;;
-    esac
-  done
-  lvm_handling
+        # FORMATTING DONE
+        while true; do
+                read -p "It seems that you've picked ${filesystem}, do you want to use LVM? [Y/n] -> " response
+                response=${response:-Y}
+                case "$response" in
+                        [yY])
+                                LVM=1
+                                break
+                                ;;            
+                        [nN])
+                                LVM=0
+                                echo -e "${C_WHITE}> ${NO_FORMAT}You won't use LVM."
+                                jump
+                                break
+                                ;;
+                        *)
+                                invalid_answer
+                        ;;
+                esac
+        done
+        lvm_handling
 }
