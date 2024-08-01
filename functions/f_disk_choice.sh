26dcc13a1f3d17bac245f600035ca5f9be6d6acf
MH3PZHARXXX001O <157519230+2ELCN0168@users.noreply.github.com>
2024-08-01 15:09:49 +0200

    Modified files

--- functions/f_disk_choice.sh
+++ functions/f_disk_choice.sh
@@ -6,45 +6,50 @@
 
 disk_choice() {
 
-  export user_disk="" # Former was finalDisk
-  export disk=""
-  export partitionType=""
-  export boot_part="" # Former was finalPartBoot
-  export root_part="" # Former was finalPartRoot
+        declare -gx user_disk="" # Former was finalDisk
+        declare -gx disk=""
+        declare -gx partitionType=""
+        declare -gx boot_part="" # Former was finalPartBoot
+        declare -gx root_part="" # Former was finalPartRoot
 
-  # finalDisk="/dev/${diskToUse}"
-  # finalPartBoot="/dev/${diskToUse}${partitionType}1"
-  # finalPartRoot="/dev/${diskToUse}${partitionType}2"
+        # finalDisk="/dev/${diskToUse}"
+        # finalPartBoot="/dev/${diskToUse}${partitionType}1"
+        # finalPartRoot="/dev/${diskToUse}${partitionType}2"
 
-  while true; do
-    printf "==DISK=============="
-    jump
-    lsblk -d --output NAME | grep -vE 'NAME|sr0|loop0'
-    printf "\n"
-    printf "====================\n"
-    read -p "Which block device do you want to use? Type it correctly -> " response
-    response=${response:-sda}
-    
-    if [[ -e /dev/$response ]]; then
-      disk="$response"
-      printf "\n"
-      printf "${C_WHITE}> ${INFO} ${NO_FORMAT}The disk to use is ${C_GREEN}/dev/${disk}${NO_FORMAT}"
-      printf "\n"
+        while true; do
+                echo -e "==DISK==============\n"
 
-      if [[ $disk =~ nvme... ]]; then 
-      partitionType="p"
-      fi
+                lsblk -d --output NAME | grep -vE 'NAME|sr0|loop0'
+                echo -e "\n"
 
-      user_disk="/dev/${disk}" # Former was finalDisk
-      boot_part="${user_disk}${partitionType}1" # Former was finalPartBoot
-      root_part="${user_disk}${partitionType}2" # Former was finalPartRoot
-      
-      break
-    
-    else
-      printf "\n"
-      invalid_answer
-      continue
-    fi
-  done
+                echo -e "====================\n"
+
+                echo -e "${B_CYAN} Which block device do you want to use? Type it correctly -> ${NO_FORMAT} \c"
+
+                declare ans_block_device="sda"
+                read ans_block_device
+                echo ""
+
+                if [[ -b "/dev/${ans_block_device}" ]]; then
+                        disk="${ans_block_device}"
+                        echo -e "\n"
+                        echo -e "${C_WHITE}> ${INFO} ${NO_FORMAT}The disk to use is ${C_GREEN}/dev/${disk}${NO_FORMAT}"
+                        echo -e "\n"
+
+                        if [[ "${disk}" =~ nvme... ]]; then 
+                                partitionType="p"
+                        fi
+
+                        user_disk="/dev/${disk}" # Former was finalDisk
+                        boot_part="${user_disk}${partitionType}1" # Former was finalPartBoot
+                        root_part="${user_disk}${partitionType}2" # Former was finalPartRoot
+
+                        break
+
+                else
+                        echo -e "\n"
+                        invalid_answer
+                        continue
+                fi
+        done
 }
