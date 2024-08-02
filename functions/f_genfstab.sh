26a85861f1536e701d99b196071726b2844f3cd3
MH3PZHARXXX001O <157519230+2ELCN0168@users.noreply.github.com>
2024-08-02 09:49:11 +0200

    Modified files

--- functions/f_genfstab.sh
+++ functions/f_genfstab.sh
@@ -1,14 +1,16 @@
+# FUNCTION(S)
+# ---
+# This function just generates the fstab file. 
+# EDIT 1 : Modified variables declarations and tests in conditions + replaced by echo.
+# ---
+
 gen_fstab() {
 
-  # Generate /etc/fstab of the new system
-  jump
-  printf "${C_WHITE}> ${INFO} ${NO_FORMAT}Generating ${C_PINK}/mnt/etc/fstab${NO_FORMAT} file."
-  jump
-  if genfstab -U /mnt >> /mnt/etc/fstab; then
-    printf "${C_WHITE}> ${SUC} ${NO_FORMAT}Generated ${C_PINK}/mnt/etc/fstab${NO_FORMAT} file."
-  else
-    printf "${C_WHITE}> ${WARN} ${NO_FORMAT}Failed to generate ${C_PINK}/mnt/etc/fstab${NO_FORMAT} file."
-  fi
-
-  jump
+        # Generate /etc/fstab of the new system
+        echo -e "${C_WHITE}> ${INFO} ${NO_FORMAT}Generating ${C_PINK}/mnt/etc/fstab${NO_FORMAT} file.\n"
+        if genfstab -U /mnt >> /mnt/etc/fstab; then
+                echo -e "${C_WHITE}> ${SUC} ${NO_FORMAT}Generated ${C_PINK}/mnt/etc/fstab${NO_FORMAT} file.\n"
+        else
+                echo -e "${C_WHITE}> ${WARN} ${NO_FORMAT}Failed to generate ${C_PINK}/mnt/etc/fstab${NO_FORMAT} file.\n"
+        fi
 }
--- functions/f_greetings.sh
+++ functions/f_greetings.sh
@@ -5,30 +5,30 @@
 # ---
 
 greetings() {
-  clear
-  echo ""
-  echo -e "${C_CYAN}     _            _     _ _                                              ${NO_FORMAT}"  
-  echo -e "${C_CYAN}    /_\  _ __ ___| |__ | (_)_ __  _   ___  __                            ${NO_FORMAT}"
-  echo -e "${C_CYAN}   //_\| '__/ __| '_ \| | | '_ \| | | \ \/ /                             ${NO_FORMAT}"
-  echo -e "${C_CYAN}  /  _  \ | | (__| | | | | | | | | |_| |>  <                             ${NO_FORMAT}"
-  echo -e "${C_CYAN}  \_/ \_/_|  \___|_| |_|_|_|_| |_|\__,_/_/\_\                            ${NO_FORMAT}"
-  echo -e "${C_CYAN}     ___           _   _         _____           _        _ _            ${NO_FORMAT}"
-  echo -e "${C_CYAN}    / _ \___ _ __ | |_| | ___    \_   \_ __  ___| |_ __ _| | | ___ _ __  ${NO_FORMAT}"
-  echo -e "${C_CYAN}   / /_\/ _ \ '_ \| __| |/ _ \    / /\/ '_ \/ __| __/ _' | | |/ _ \ '__| ${NO_FORMAT}"
-  echo -e "${C_CYAN}  / /_\\  __/ | | | |_| |  __/ /\/ /_ | | | \__ \ || (_| | | |  __/ |    ${NO_FORMAT}"
-  echo -e "${C_CYAN}  \____/\___|_| |_|\__|_|\___| \____/ |_| |_|___/\__\__,_|_|_|\___|_|    ${NO_FORMAT}"
-                                                                      
+        clear
+        echo ""
+        echo -e "${C_CYAN}  █████╗ ██████╗  ██████╗██╗  ██╗     ██████╗ ███████╗███╗   ██╗████████╗██╗     ███████╗${NO_FORMAT}" 
+        echo -e "${C_CYAN} ██╔══██╗██╔══██╗██╔════╝██║  ██║    ██╔════╝ ██╔════╝████╗  ██║╚══██╔══╝██║     ██╔════╝${NO_FORMAT}" 
+        echo -e "${C_CYAN} ███████║██████╔╝██║     ███████║    ██║  ███╗█████╗  ██╔██╗ ██║   ██║   ██║     █████╗  ${NO_FORMAT}" 
+        echo -e "${C_CYAN} ██╔══██║██╔══██╗██║     ██╔══██║    ██║   ██║██╔══╝  ██║╚██╗██║   ██║   ██║     ██╔══╝  ${NO_FORMAT}" 
+        echo -e "${C_CYAN} ██║  ██║██║  ██║╚██████╗██║  ██║    ╚██████╔╝███████╗██║ ╚████║   ██║   ███████╗███████╗${NO_FORMAT}" 
+        echo -e "${C_CYAN} ╚═╝  ╚═╝╚═╝  ╚═╝ ╚═════╝╚═╝  ╚═╝     ╚═════╝ ╚══════╝╚═╝  ╚═══╝   ╚═╝   ╚══════╝╚══════╝${NO_FORMAT}" 
+        echo -e "${C_CYAN}                                                                                         ${NO_FORMAT}" 
+        echo -e "${C_CYAN}         ██╗███╗   ██╗███████╗████████╗ █████╗ ██╗     ██╗     ███████╗██████╗           ${NO_FORMAT}" 
+        echo -e "${C_CYAN}         ██║████╗  ██║██╔════╝╚══██╔══╝██╔══██╗██║     ██║     ██╔════╝██╔══██╗          ${NO_FORMAT}" 
+        echo -e "${C_CYAN}         ██║██╔██╗ ██║███████╗   ██║   ███████║██║     ██║     █████╗  ██████╔╝          ${NO_FORMAT}" 
+        echo -e "${C_CYAN}         ██║██║╚██╗██║╚════██║   ██║   ██╔══██║██║     ██║     ██╔══╝  ██╔══██╗          ${NO_FORMAT}" 
+        echo -e "${C_CYAN}         ██║██║ ╚████║███████║   ██║   ██║  ██║███████╗███████╗███████╗██║  ██║          ${NO_FORMAT}" 
+        echo -e "${C_CYAN}         ╚═╝╚═╝  ╚═══╝╚══════╝   ╚═╝   ╚═╝  ╚═╝╚══════╝╚══════╝╚══════╝╚═╝  ╚═╝          ${NO_FORMAT}" 
 
-  printf "\n${C_CYAN}███${C_RED}█${C_CYAN}█${C_YELLOW}█${C_CYAN}█${NO_FORMAT}"
-  jump
 
-  date
-  printf "${C_CYAN}> ${C_WHITE}Welcome to this gently automated ${C_CYAN}Arch/\Linux${NO_FORMAT} ${C_WHITE}installer. ${C_CYAN}<${NO_FORMAT}"
-  jump
+        echo -e "\n${C_CYAN}███${C_RED}█${C_CYAN}█${C_YELLOW}█${C_CYAN}█${NO_FORMAT}\n"
 
-  printf "${C_WHITE}> ${C_PINK}Before starting, make sure you have ${C_RED}no LVM ${C_PINK}configured on your disk, or it will ${C_RED}mess up${C_PINK} the script. You must delete any LV, VG and PV before starting.${NO_FORMAT}"
-  jump
+        date
+        echo -e "${C_CYAN}> ${C_WHITE}Welcome to this gently automated ${C_CYAN}Arch/\Linux${NO_FORMAT} ${C_WHITE}installer. ${C_CYAN}<${NO_FORMAT}\n"
 
-  # This unmounting action ensure to have nothing actually mounted on /mnt before starting
-  umount -R /mnt &> /dev/null
+        echo -e "${C_WHITE}> ${C_PINK}Before starting, make sure you have ${C_RED}no LVM ${C_PINK}configured on your disk, or it will ${C_RED}mess up${C_PINK} the script. You must delete any LV, VG and PV before starting.${NO_FORMAT}\n"
+
+        # This unmounting action ensure to have nothing actually mounted on /mnt before starting
+        umount -R /mnt &> /dev/null
 }
