5c5e88f5621fff51590f23334bf1858973ef3efe
2ELCN0168 <lcn.en.perso@protonmail.com>
2024-08-03 17:04:13 +0200

    Modified files

--- functions/f_bootloader_choice.sh
+++ functions/f_bootloader_choice.sh
@@ -23,29 +23,29 @@ bootloader_choice() {
                         
                         echo -e "${C_CYAN}${BOLD}:: ${C_WHITE}Which one do you prefer? [0/1/2] -> ${NO_FORMAT} \c"
 
-                        declare -i ans_bootloader="0"
+                        declare ans_bootloader=""
                         read ans_bootloader
                         echo ""
-                        #response=${response:-0}
+                        response=${response:=0}
                         case "${ans_bootloader}" in
-                                [0])
+                                0)
                                         echo -e "${C_WHITE}> ${INFO} We will install ${C_CYAN}rEFInd${NO_FORMAT}\n"
                                         bootloader="REFIND"
                                         break
                                         ;;
-                                [1])
+                                1)
                                         echo -e "${C_WHITE}> ${INFO} We will install ${C_YELLOW}GRUB2${NO_FORMAT}\n"
                                         bootloader="GRUB"
                                         break
                                         ;;
-                                [2])
+                                2)
                                         echo -e "${C_WHITE}> ${INFO} We will install ${C_RED}systemd-boot${NO_FORMAT}\n"
                                         bootloader="SYSTEMDBOOT"
                                         break
                                         ;;
                                 *)
                                         invalid_answer
-                                ;;
+                                        ;;
                         esac
                 done
         elif [[ "${UEFI}" -eq 0 ]]; then
