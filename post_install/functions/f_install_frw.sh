353b92f36a48c198975b11a8f592e3607ec5ba19
MH3PZHARXXX001O <157519230+2ELCN0168@users.noreply.github.com>
2024-09-23 11:13:20 +0200

    Modified files

--- post_install/functions/f_install_frw.sh
+++ post_install/functions/f_install_frw.sh
@@ -5,9 +5,9 @@ install_frw() {
         # Install nftables, but it should already be there
         echo -e "${C_WHITE}> ${INFO} Installing ${C_WHITE}nftables.${NO_FORMAT}"
 
-        if ! systemctl is-enabled nftables; then
-                if ! pacman -Qi nftables; then
-                        pacman -S nftables --noconfirm
+        if ! systemctl is-enabled nftables 1> "/dev/null" 2>&1; then
+                if ! pacman -Qi nftables 1> "/dev/null" 2>&1; then
+                        pacman -S nftables --noconfirm 1> "/dev/null" 2>&1
                         if [[ "${?}" -eq 0 ]]; then
                                 echo -e "${C_WHITE}> ${SUC} Installed ${C_WHITE}nftables.${NO_FORMAT}\n"
                         else
@@ -15,22 +15,21 @@ install_frw() {
                         fi
                 fi
 
+                echo -e "${C_WHITE}> ${INFO} ${C_WHITE}systemctl ${C_GREEN}enable${C_WHITE} nftables.${NO_FORMAT}"
+
                 if systemctl enable nftables 1> "/dev/null" 2>&1; then
-                        echo -e "${C_WHITE}> ${INFO} ${C_WHITE}systemctl ${C_GREEN}enable${C_WHITE} nftables.${NO_FORMAT}\n"
+                        echo -e "${C_WHITE}> ${INFO} nftables is ${C_GREEN}enabled.${NO_FORMAT}\n"
                 else
                         echo -e "${C_WHITE}> ${ERR} Cannot enable ${C_WHITE}nftables.${NO_FORMAT}\n"
                 fi
-                echo -e "${C_WHITE}> ${INFO} ${C_WHITE}nftables is already ${C_GREEN}enabled.${NO_FORMAT}\n"
         fi
                                 
         # Install sshguard (fail2ban like)
         echo -e "${C_WHITE}> ${INFO} Installing ${C_WHITE}sshguard.${NO_FORMAT}"
 
-        if ! systemctl is-enabled sshguard #1> "/dev/null" 2>&1
-                then
-                if ! pacman -Qi sshguard # 1> "/dev/null" 2>&1; 
-                then
-                        pacman -S sshguard --noconfirm #1> "/dev/null" 2>&1
+        if ! systemctl is-enabled sshguard 1> "/dev/null" 2>&1; then
+                if ! pacman -Qi sshguard 1> "/dev/null" 2>&1; then
+                        pacman -S sshguard --noconfirm 1> "/dev/null" 2>&1
                         if [[ "${?}" -eq 0 ]]; then
                                 echo -e "${C_WHITE}> ${SUC} Installed ${C_WHITE}sshguard.${NO_FORMAT}"
                         else
@@ -38,8 +37,10 @@ install_frw() {
                         fi
                 fi
                                 
+                echo -e "${C_WHITE}> ${INFO} ${C_WHITE}systemctl ${C_GREEN}enable${C_WHITE} sshguard.${NO_FORMAT}\n"
+
                 if systemctl enable sshguard 1> "/dev/null" 2>&1; then
-                        echo -e "${C_WHITE}> ${INFO} ${C_WHITE}systemctl ${C_GREEN}enable${C_WHITE} sshguard.${NO_FORMAT}\n"
+                        echo -e "${C_WHITE}> ${INFO} sshguard is ${C_GREEN}enabled.${NO_FORMAT}\n"
                 else
                         echo -e "${C_WHITE}> ${ERR} Cannot enable ${C_WHITE}sshguard.${NO_FORMAT}\n"
                 fi
