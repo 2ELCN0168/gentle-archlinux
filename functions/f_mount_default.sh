d5e43ba54df8ba54b1a1906a46813f8cf6842ee8
MH3PZHARXXX001O <157519230+2ELCN0168@users.noreply.github.com>
2024-09-24 12:01:48 +0200

    Modified files

--- functions/f_mount_default.sh
+++ functions/f_mount_default.sh
@@ -4,7 +4,7 @@ mount_default() {
 
         # FORMATTING DONE
         echo -e "${C_WHITE}> ${NO_FORMAT}Mounting ${C_GREEN}${root_part}${NO_FORMAT} to /mnt\n"
-        mount "${root_part}" "/mnt" 
+        # mount "${root_part}" "/mnt" 
 
         echo -e "${C_WHITE}> ${NO_FORMAT}Mounting ${C_GREEN}${boot_part}${NO_FORMAT} to /mnt/boot\n"
         mount --mkdir "${boot_part}" "/mnt/boot"
