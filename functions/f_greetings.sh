9c0551548a62438379769b1adde5ff18da60fc8d
MH3PZHARXXX001O <157519230+2ELCN0168@users.noreply.github.com>
2024-09-24 14:20:43 +0200

    Modified files

--- functions/f_greetings.sh
+++ functions/f_greetings.sh
@@ -61,10 +61,11 @@ greetings() {
         echo -e "${C_WHITE}> ${C_PINK}Before starting, make sure you have ${C_RED}no LVM ${C_PINK}configured on your disk, or it will ${C_RED}mess up${C_PINK} the script. You must delete any LV, VG and PV before starting.${NO_FORMAT}\n"
 
         # This unmounting action ensure to have nothing actually mounted on /mnt before starting
-        for i in "/mnt/*"; do
+        for i in /mnt/*; do
                 if mountpoint -q "${i}"; then
                         umount -R "${i}" 1> "/dev/null" 2>&1
-                        echo "{$i}"
+                        echo "${i}"
                 fi
         done
+        umount -R "/mnt" 1> "/dev/null" 2>&1
 }
