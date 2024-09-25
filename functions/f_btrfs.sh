2e2dda34e6723fd8f060b23df298c4769b86df6e
MH3PZHARXXX001O <157519230+2ELCN0168@users.noreply.github.com>
2024-09-25 13:34:30 +0200

    Modified files

--- functions/f_btrfs.sh
+++ functions/f_btrfs.sh
@@ -62,82 +62,81 @@ btrfs() {
         if [[ "${btrfsSubvols}" -eq 0 ]]; then
                 mount_default
                 return
-        elif [[ "${btrfsSubvols}" -eq 1 ]]; then
-
-                echo -e "root part is ${root_part}"
-                if mountpoint -q "/mnt"; then
-                        umount -R "/mnt"
-                        if ! mount "${root_part}" "/mnt"; then #1> "/dev/null" 2>&1
-                                echo -e "Cannot mount ${root_part} to /mnt"
-                                exit 1
-                        else
-                                echo "mounted ${root_part} to /mnt"
-                        fi
-                fi
-
-                # for i in "${btrfs_subvols[@]}"; do
-                        # echo -e "${C_WHITE}> ${INFO} Creating${NO_FORMAT} ${C_YELLOW}subvolume ${C_GREEN}${i}${NO_FORMAT}"
-                #         # if ! btrfs subvolume create "/mnt/${r}"; then # 1> "/dev/null" 2>&1
-                #         #         echo "Cannot create subvolume ${r}"
-                #         # fi
-                #         # if [[ "${?}" -ne 0 ]]; then
-                #         #         echo "Something went wrong"
-                #         # else
-                #         #         echo "Created subvolume ${i}"
-                #         # fi
-                # done
-                # 
-                # if ! btrfs subvolume create "/mnt/${btrfs_subvols[0]}"; then
-                #         echo -e "Error while creating the subvolumes. Exiting."
-                #         exit 1
-                # fi
-                # if ! btrfs subvolume create "/mnt/${btrfs_subvols[1]}"; then
-                #         echo -e "Error while creating the subvolumes. Exiting."
-                #         exit 1
-                # fi
-                # if ! btrfs subvolume create "/mnt/${btrfs_subvols[2]}"; then
-                #         echo -e "Error while creating the subvolumes. Exiting."
-                #         exit 1
-                # fi
-                # if ! btrfs subvolume create "/mnt/${btrfs_subvols[3]}"; then
-                #         echo -e "Error while creating the subvolumes. Exiting."
-                #         exit 1
-                # fi
-                # if ! btrfs subvolume create "/mnt/${btrfs_subvols[4]}"; then
-                #         echo -e "Error while creating the subvolumes. Exiting."
-                #         exit 1
-                # fi
-
-                btrfs subvolume create /mnt/@
-                btrfs subvolume create /mnt/@home
-                btrfs subvolume create /mnt/@usr
-                btrfs subvolume create /mnt/@var
-                btrfs subvolume create /mnt/@tmp
-                echo ""
-
-                umount -R "/mnt" 1> "/dev/null" 2>&1
-
-                echo -e "${C_WHITE}> ${INFO} Mounting ${C_GREEN}@${NO_FORMAT} to ${C_WHITE}/mnt${NO_FORMAT}"
-                mount -t btrfs -o compress=zstd,discard=async,autodefrag,subvol=@ "${root_part}" "/mnt"
-
-                echo -e "${C_WHITE}> ${INFO} Mounting ${C_GREEN}@home${NO_FORMAT} to ${C_WHITE}/mnt/home${NO_FORMAT}"
-                mount --mkdir -t btrfs -o compress=zstd,discard=async,autodefrag,subvol=@home "${root_part}" "/mnt/home"
-
-                echo -e "${C_WHITE}> ${INFO} Mounting ${C_GREEN}@usr${NO_FORMAT} to ${C_WHITE}/mnt/usr${NO_FORMAT}"
-                mount --mkdir -t btrfs -o compress=zstd,discard=async,autodefrag,subvol=@usr "${root_part}" "/mnt/usr"
-
-                echo -e "${C_WHITE}> ${INFO} Mounting ${C_GREEN}@tmp${NO_FORMAT} to ${C_WHITE}/mnt/tmp${NO_FORMAT}"
-                mount --mkdir -t btrfs -o compress=zstd,discard=async,autodefrag,subvol=@tmp "${root_part}" "/mnt/tmp"
-
-                echo -e "${C_WHITE}> ${INFO} Mounting ${C_GREEN}@var${NO_FORMAT} to ${C_WHITE}/mnt/var${NO_FORMAT}"
-                mount --mkdir -t btrfs -o compress=zstd,discard=async,autodefrag,subvol=@var "${root_part}" "/mnt/var"
-
-                echo -e "${C_WHITE}> ${INFO} Mounting ${C_GREEN}/dev/sda1${NO_FORMAT} to ${C_WHITE}/mnt/boot${NO_FORMAT}\n"
-                mount --mkdir "${boot_part}" "/mnt/boot"
-                echo ""
-
-                lsblk -f
         fi
+
+        echo -e "root part is ${root_part}"
+        if mountpoint -q "/mnt"; then
+                umount -R "/mnt"
+                if ! mount "${root_part}" "/mnt"; then #1> "/dev/null" 2>&1
+                        echo -e "Cannot mount ${root_part} to /mnt"
+                        exit 1
+                else
+                        echo "mounted ${root_part} to /mnt"
+                fi
+        fi
+
+        # for i in "${btrfs_subvols[@]}"; do
+                # echo -e "${C_WHITE}> ${INFO} Creating${NO_FORMAT} ${C_YELLOW}subvolume ${C_GREEN}${i}${NO_FORMAT}"
+        #         # if ! btrfs subvolume create "/mnt/${r}"; then # 1> "/dev/null" 2>&1
+        #         #         echo "Cannot create subvolume ${r}"
+        #         # fi
+        #         # if [[ "${?}" -ne 0 ]]; then
+        #         #         echo "Something went wrong"
+        #         # else
+        #         #         echo "Created subvolume ${i}"
+        #         # fi
+        # done
+        # 
+        # if ! btrfs subvolume create "/mnt/${btrfs_subvols[0]}"; then
+        #         echo -e "Error while creating the subvolumes. Exiting."
+        #         exit 1
+        # fi
+        # if ! btrfs subvolume create "/mnt/${btrfs_subvols[1]}"; then
+        #         echo -e "Error while creating the subvolumes. Exiting."
+        #         exit 1
+        # fi
+        # if ! btrfs subvolume create "/mnt/${btrfs_subvols[2]}"; then
+        #         echo -e "Error while creating the subvolumes. Exiting."
+        #         exit 1
+        # fi
+        # if ! btrfs subvolume create "/mnt/${btrfs_subvols[3]}"; then
+        #         echo -e "Error while creating the subvolumes. Exiting."
+        #         exit 1
+        # fi
+        # if ! btrfs subvolume create "/mnt/${btrfs_subvols[4]}"; then
+        #         echo -e "Error while creating the subvolumes. Exiting."
+        #         exit 1
+        # fi
+
+        btrfs subvolume create /mnt/@
+        btrfs subvolume create /mnt/@home
+        btrfs subvolume create /mnt/@usr
+        btrfs subvolume create /mnt/@var
+        btrfs subvolume create /mnt/@tmp
+        echo ""
+
+        umount -R "/mnt" 1> "/dev/null" 2>&1
+
+        echo -e "${C_WHITE}> ${INFO} Mounting ${C_GREEN}@${NO_FORMAT} to ${C_WHITE}/mnt${NO_FORMAT}"
+        mount -t btrfs -o compress=zstd,discard=async,autodefrag,subvol=@ "${root_part}" "/mnt"
+
+        echo -e "${C_WHITE}> ${INFO} Mounting ${C_GREEN}@home${NO_FORMAT} to ${C_WHITE}/mnt/home${NO_FORMAT}"
+        mount --mkdir -t btrfs -o compress=zstd,discard=async,autodefrag,subvol=@home "${root_part}" "/mnt/home"
+
+        echo -e "${C_WHITE}> ${INFO} Mounting ${C_GREEN}@usr${NO_FORMAT} to ${C_WHITE}/mnt/usr${NO_FORMAT}"
+        mount --mkdir -t btrfs -o compress=zstd,discard=async,autodefrag,subvol=@usr "${root_part}" "/mnt/usr"
+
+        echo -e "${C_WHITE}> ${INFO} Mounting ${C_GREEN}@tmp${NO_FORMAT} to ${C_WHITE}/mnt/tmp${NO_FORMAT}"
+        mount --mkdir -t btrfs -o compress=zstd,discard=async,autodefrag,subvol=@tmp "${root_part}" "/mnt/tmp"
+
+        echo -e "${C_WHITE}> ${INFO} Mounting ${C_GREEN}@var${NO_FORMAT} to ${C_WHITE}/mnt/var${NO_FORMAT}"
+        mount --mkdir -t btrfs -o compress=zstd,discard=async,autodefrag,subvol=@var "${root_part}" "/mnt/var"
+
+        echo -e "${C_WHITE}> ${INFO} Mounting ${C_GREEN}/dev/sda1${NO_FORMAT} to ${C_WHITE}/mnt/boot${NO_FORMAT}\n"
+        mount --mkdir "${boot_part}" "/mnt/boot"
+        echo ""
+
+        lsblk -f
 
 
         if [[ "${btrfsQuotas}" -eq 1 ]]; then
