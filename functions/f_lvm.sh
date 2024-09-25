0747a8068a5f6a689cf55f4edcce82f210d4cad3
MH3PZHARXXX001O <157519230+2ELCN0168@users.noreply.github.com>
2024-09-25 16:10:01 +0200

    Modified files

--- functions/f_lvm.sh
+++ functions/f_lvm.sh
@@ -34,6 +34,8 @@
 lvm() {
 
         # FORMATTING DONE
+
+        local logical_volumes=("root" "usr" "home" "var" "tmp")
         
         if [[ "${LVM}" -eq 0 ]]; then
                 echo -e "${C_WHITE}> ${INFO} ${C_CYAN}Formatting ${root_part} to ${filesystem}.${NO_FORMAT}\n"
@@ -76,39 +78,68 @@ lvm() {
                 lvcreate -l 10%VG VG_Archlinux -n var
                 lvcreate -l 10%VG VG_Archlinux -n tmp
 
-                case "${filesystem}" in
-                        "XFS")
-                                #TODO: Make a loop for and complete names with vars
-                                
-                                mkfs.xfs -f -L Arch_root "/dev/mapper/VG_Archlinux-root" 1> "/dev/null" 2>&1
-                                mkfs.xfs -f -L Arch_home "/dev/mapper/VG_Archlinux-home" 1> "/dev/null" 2>&1
-                                mkfs.xfs -f -L Arch_usr "/dev/mapper/VG_Archlinux-usr" 1> "/dev/null" 2>&1
-                                mkfs.xfs -f -L Arch_var "/dev/mapper/VG_Archlinux-var" 1> "/dev/null" 2>&1
-                                mkfs.xfs -f -L Arch_tmp "/dev/mapper/VG_Archlinux-tmp" 1> "/dev/null" 2>&1
-                                ;;
-                        "EXT4")
-                                mkfs.ext4 -L Arch_root "/dev/mapper/VG_Archlinux-root" 1> "/dev/null" 2>&1
-                                mkfs.ext4 -L Arch_home "/dev/mapper/VG_Archlinux-home" 1> "/dev/null" 2>&1
-                                mkfs.ext4 -L Arch_usr "/dev/mapper/VG_Archlinux-usr" 1> "/dev/null" 2>&1
-                                mkfs.ext4 -L Arch_var "/dev/mapper/VG_Archlinux-var" 1> "/dev/null" 2>&1
-                                mkfs.ext4 -L Arch_tmp "/dev/mapper/VG_Archlinux-tmp" 1> "/dev/null" 2>&1
-                                ;;
-                esac 
+                local fs=""
 
-                echo -e "${C_WHITE}> ${INFO} Mounting ${C_CYAN}VG_Archlinux-root${NO_FORMAT} to /mnt\n"
-                mount "/dev/mapper/VG_Archlinux-root" "/mnt"
+                for i in "${logical_volumes[@]}"; do
+                        # lvcreate -l ${logical_volumes} VG_Archlinux -n ${i} 1> "/dev/null" 2>&1
+                        case "${filesystem}" in
+                                "XFS")
+                                        fs="xfs"
+                                        ;;
+                                "EXT4")
+                                        fs=ext4
+                                        ;;
+                        esac
+                        mkfs.${fs} -L Arch_${i} "/dev/mapper/VG_Archlinux-${i}" 1> "/dev/null" 2>&1
+                        echo -e "${C_WHITE}> ${INFO} Mounting ${C_CYAN}VG_Archlinux-${i}${NO_FORMAT} to /mnt/${i}"
+                        if [[ "${i}" == "root" ]]; then
+                                mount --mkdir "/dev/mapper/VG_Archlinux-${i}" "/mnt"
+                        else 
+                                mount --mkdir "/dev/mapper/VG_Archlinux-${i}" "/mnt/${i}"
+                        fi
 
-                echo -e "${C_WHITE}> ${INFO} Mounting ${C_CYAN}VG_Archlinux-home${NO_FORMAT} to /mnt/home\n"
-                mount --mkdir "/dev/mapper/VG_Archlinux-home" "/mnt/home"
+                        if [[ "${?}" -ne 0 ]]; then
+                                echo -e "${C_WHITE}> ${ERR} Error mounting ${C_CYAN}VG_Archlinux-${i}${NO_FORMAT} to /mnt/${i}"
+                                exit 1
+                        fi
+                done
 
-                echo -e "${C_WHITE}> ${INFO} Mounting ${C_CYAN}VG_Archlinux-usr${NO_FORMAT} to /mnt/usr\n"
-                mount --mkdir "/dev/mapper/VG_Archlinux-usr" "/mnt/usr"
 
-                echo -e "${C_WHITE}> ${INFO} Mounting ${C_CYAN}VG_Archlinux-var${NO_FORMAT} to /mnt/var\n"
-                mount --mkdir "/dev/mapper/VG_Archlinux-var" "/mnt/var"
 
-                echo -e "${C_WHITE}> ${INFO} Mounting ${C_CYAN}VG_Archlinux-tmp${NO_FORMAT} to /mnt/tmp\n"
-                mount --mkdir "/dev/mapper/VG_Archlinux-tmp" "/mnt/tmp"
+
+                # case "${filesystem}" in
+                #         "XFS")
+                #                 #TODO: Make a loop for and complete names with vars
+                #                 
+                #                 mkfs.xfs -f -L Arch_root "/dev/mapper/VG_Archlinux-root" 1> "/dev/null" 2>&1
+                #                 mkfs.xfs -f -L Arch_home "/dev/mapper/VG_Archlinux-home" 1> "/dev/null" 2>&1
+                #                 mkfs.xfs -f -L Arch_usr "/dev/mapper/VG_Archlinux-usr" 1> "/dev/null" 2>&1
+                #                 mkfs.xfs -f -L Arch_var "/dev/mapper/VG_Archlinux-var" 1> "/dev/null" 2>&1
+                #                 mkfs.xfs -f -L Arch_tmp "/dev/mapper/VG_Archlinux-tmp" 1> "/dev/null" 2>&1
+                #                 ;;
+                #         "EXT4")
+                #                 mkfs.ext4 -L Arch_root "/dev/mapper/VG_Archlinux-root" 1> "/dev/null" 2>&1
+                #                 mkfs.ext4 -L Arch_home "/dev/mapper/VG_Archlinux-home" 1> "/dev/null" 2>&1
+                #                 mkfs.ext4 -L Arch_usr "/dev/mapper/VG_Archlinux-usr" 1> "/dev/null" 2>&1
+                #                 mkfs.ext4 -L Arch_var "/dev/mapper/VG_Archlinux-var" 1> "/dev/null" 2>&1
+                #                 mkfs.ext4 -L Arch_tmp "/dev/mapper/VG_Archlinux-tmp" 1> "/dev/null" 2>&1
+                #                 ;;
+                # esac 
+
+                # echo -e "${C_WHITE}> ${INFO} Mounting ${C_CYAN}VG_Archlinux-root${NO_FORMAT} to /mnt\n"
+                # mount "/dev/mapper/VG_Archlinux-root" "/mnt"
+                #
+                # echo -e "${C_WHITE}> ${INFO} Mounting ${C_CYAN}VG_Archlinux-home${NO_FORMAT} to /mnt/home\n"
+                # mount --mkdir "/dev/mapper/VG_Archlinux-home" "/mnt/home"
+                #
+                # echo -e "${C_WHITE}> ${INFO} Mounting ${C_CYAN}VG_Archlinux-usr${NO_FORMAT} to /mnt/usr\n"
+                # mount --mkdir "/dev/mapper/VG_Archlinux-usr" "/mnt/usr"
+                #
+                # echo -e "${C_WHITE}> ${INFO} Mounting ${C_CYAN}VG_Archlinux-var${NO_FORMAT} to /mnt/var\n"
+                # mount --mkdir "/dev/mapper/VG_Archlinux-var" "/mnt/var"
+                #
+                # echo -e "${C_WHITE}> ${INFO} Mounting ${C_CYAN}VG_Archlinux-tmp${NO_FORMAT} to /mnt/tmp\n"
+                # mount --mkdir "/dev/mapper/VG_Archlinux-tmp" "/mnt/tmp"
 
                 echo -e "${C_WHITE}> ${INFO} Mounting ${C_CYAN}${boot_part}${NO_FORMAT} to /mnt/boot\n"
                 mount --mkdir "${boot_part}" "/mnt/boot"
