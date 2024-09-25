e728892586027f21ae30c1879c11ea0a149a49a7
2ELCN0168 <lcn.en.perso@protonmail.com>
2024-09-25 20:38:12 +0200

    Modified files

--- functions/f_mount_default.sh
+++ functions/f_mount_default.sh
@@ -5,9 +5,9 @@ mount_default() {
         # FORMATTING DONE
         echo -e "${C_WHITE}> ${INFO} Mounting ${C_GREEN}${root_part}${NO_FORMAT} to /mnt"
         if mount "${root_part}" "/mnt" 1> "/dev/null" 2>&1; then
-                echo -e "${C_WHITE}> ${SUC} Mounted ${C_GREEN}${boot_part}${NO_FORMAT} to /mnt/boot\n"
+                echo -e "${C_WHITE}> ${SUC} Mounted ${C_GREEN}${root_part}${NO_FORMAT} to /mnt/boot\n"
         else
-                echo -e "${C_WHITE}> ${ERR} Error while mounting ${C_GREEN}${boot_part}${NO_FORMAT} to /mnt/boot\n"
+                echo -e "${C_WHITE}> ${ERR} Error while mounting ${C_GREEN}${root_part}${NO_FORMAT} to /mnt/boot\n"
                 exit 1
         fi
 
