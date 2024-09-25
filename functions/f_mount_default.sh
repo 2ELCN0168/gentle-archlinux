39a501741f8c2aafac38be5d93c884d40adf961e
2ELCN0168 <lcn.en.perso@protonmail.com>
2024-09-25 20:19:29 +0200

    Modified files

--- functions/f_mount_default.sh
+++ functions/f_mount_default.sh
@@ -3,9 +3,19 @@
 mount_default() {
 
         # FORMATTING DONE
-        echo -e "${C_WHITE}> ${NO_FORMAT}Mounting ${C_GREEN}${root_part}${NO_FORMAT} to /mnt\n"
-        mount "${root_part}" "/mnt" 
+        echo -e "${C_WHITE}> ${INFO} Mounting ${C_GREEN}${root_part}${NO_FORMAT} to /mnt"
+        if mount "${root_part}" "/mnt" 1> "/dev/null" 2>&1; then
+                echo -e "${C_WHITE}> ${SUC} Mounted ${C_GREEN}${boot_part}${NO_FORMAT} to /mnt/boot\n"
+        else
+                echo -e "${C_WHITE}> ${ERR} Error while mounting ${C_GREEN}${boot_part}${NO_FORMAT} to /mnt/boot\n"
+                exit 1
+        fi
 
-        echo -e "${C_WHITE}> ${NO_FORMAT}Mounting ${C_GREEN}${boot_part}${NO_FORMAT} to /mnt/boot\n"
-        mount --mkdir "${boot_part}" "/mnt/boot"
+        echo -e "${C_WHITE}> ${INFO} Mounting ${C_GREEN}${boot_part}${NO_FORMAT} to /mnt/boot"
+        if mount --mkdir "${boot_part}" "/mnt/boot" 1> "/dev/null" 2>&1
+                echo -e "${C_WHITE}> ${SUC} Mounted ${C_GREEN}${boot_part}${NO_FORMAT} to /mnt/boot\n"
+        else
+                echo -e "${C_WHITE}> ${ERR} Error while mounting ${C_GREEN}${boot_part}${NO_FORMAT} to /mnt/boot\n"
+                exit 1
+        fi
 }
