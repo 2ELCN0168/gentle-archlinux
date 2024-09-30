4f35ab890984d0af6855d2b85d185d381e0089bd
2ELCN0168 <lcn.en.perso@protonmail.com>
2024-09-30 20:53:41 +0200

    Reformatted file

--- functions/f_genfstab.sh
+++ functions/f_genfstab.sh
@@ -1,16 +1,34 @@
-# FUNCTION(S)
-# ---
+#
+### File: f_genfstab.sh
+#
+### Description: 
 # This function just generates the fstab file. 
-# EDIT 1 : Modified variables declarations and tests in conditions + replaced by echo.
-# ---
+#
+### Author: 2ELCN0168
+# Last updated: 2024-09-30
+#
+### Dependencies:
+# - arch-install-scripts (included in archiso).
+#
+### Usage:
+#
+# 1. Generate fstab file for everything mounted in "/mnt".
+#
+# NOTE:
+# Long parameters for "genfstab" don't exist.
+#
 
 gen_fstab() {
 
-        # Generate /etc/fstab of the new system
-        echo -e "${C_WHITE}> ${INFO} ${NO_FORMAT}Generating ${C_PINK}/mnt/etc/fstab${NO_FORMAT} file."
-        if genfstab -U /mnt >> /mnt/etc/fstab; then
-                echo -e "${C_WHITE}> ${SUC} ${NO_FORMAT}Generated ${C_PINK}/mnt/etc/fstab${NO_FORMAT} file.\n"
+        echo -e "${C_WHITE}> ${INFO} ${NO_FORMAT}Generating" \
+                "${C_PINK}/mnt/etc/fstab${NO_FORMAT} file."
+        # COMMAND:
+        # genftab --uuid "/mnt"
+        if genfstab -U "/mnt" >> "/mnt/etc/fstab"; then
+                echo -e "${C_WHITE}> ${SUC} ${NO_FORMAT}Generated" \
+                        "${C_PINK}/mnt/etc/fstab${NO_FORMAT} file.\n"
         else
-                echo -e "${C_WHITE}> ${WARN} ${NO_FORMAT}Failed to generate ${C_PINK}/mnt/etc/fstab${NO_FORMAT} file.\n"
+                echo -e "${C_WHITE}> ${WARN} ${NO_FORMAT}Failed to generate" \
+                        "${C_PINK}/mnt/etc/fstab${NO_FORMAT} file.\n"
         fi
 }
