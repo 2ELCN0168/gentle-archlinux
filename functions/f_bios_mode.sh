e8165b9a530a5b70cef8f5f641503d27fb05ecd5
MH3PZHARXXX001O <157519230+2ELCN0168@users.noreply.github.com>
2024-09-30 14:42:03 +0200

    Corrected if condition and reformatted file

--- functions/f_bios_mode.sh
+++ functions/f_bios_mode.sh
@@ -26,17 +26,17 @@ get_bios_mode() {
         export UEFI=0
         local efi_path="/sys/firmware/efi/fw_platform_size"
 
-        if [[ -z "${efi_path}" ]]; then
-                echo -e "${C_WHITE}> ${INFO} ${NO_FORMAT}Running in " \
+        if [[ ! -z "${efi_path}" ]]; then
+                echo -e "${C_WHITE}> ${INFO} ${NO_FORMAT}Running in" \
                         "${C_CYAN}UEFI${NO_FORMAT} mode."
-                echo -e "${C_CYAN}You are using UEFI mode, you have the choice " \
+                echo -e "${C_CYAN}You are using UEFI mode, you have the choice" \
                         "for the bootloader...${NO_FORMAT}\n"
                 UEFI=1
         else
-                echo -e "${C_WHITE}> ${INFO} ${NO_FORMAT}Running in " \
+                echo -e "${C_WHITE}> ${INFO} ${NO_FORMAT}Running in" \
                         "${C_RED}BIOS${NO_FORMAT} mode."
-                echo -e "${C_YELLOW}No choice for you. You would have been " \
-                        "better off using UEFI mode. We will install " \
+                echo -e "${C_YELLOW}No choice for you. You would have been" \
+                        "better off using UEFI mode. We will install" \
                         "GRUB2.${NO_FORMAT}\n"
                 UEFI=0
         fi
