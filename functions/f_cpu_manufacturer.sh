a5e444d09b5ce00f672e986a5c790a585c6d7f55
MH3PZHARXXX001O <157519230+2ELCN0168@users.noreply.github.com>
2024-09-30 14:00:54 +0200

    Reformatted file

--- functions/f_cpu_manufacturer.sh
+++ functions/f_cpu_manufacturer.sh
@@ -1,27 +1,43 @@
-# FUNCTION(S)
-# ---
-# This function aims to get the CPU manufacturer to install the apropriate microcode in a later stage.
-# EDIT 1 : Modified variables declarations and tests in conditions + replaced by echo.
-# ---
+#
+### File: f_cpu_manufacturer.sh
+#
+### Description: 
+# Detect the CPU vendor to install the correct microcode at the system installation
+#
+### Author: 2ELCN0168
+# Last updated: 2024-09-30
+#
+### Dependencies:
+# - none.
+#
+### Usage:
+#
+# 1. Grep to lscpu.
+#
+
 
 get_cpu_brand() {
 
-        vendor=""
+        local vendor=""
         export cpuBrand=""
 
-        vendor="$(lscpu | grep -i vendor | awk '{ print $3 }' | head -1)"
+        vendor="$(lscpu | grep -i "vendor" | awk '{ print $3 }' | head -1)"
 
         case "${vendor}" in
                 "GenuineIntel")
-                        echo -e "${C_WHITE}> ${INFO} ${C_CYAN}INTEL CPU${NO_FORMAT} detected.\n"
+                        echo -e "${C_WHITE}> ${INFO} ${C_CYAN}INTEL CPU" \
+                                "${NO_FORMAT} detected.\n"
                         cpuBrand="INTEL"
                         ;;
                 "AuthenticAMD")
-                        echo -e "${C_WHITE}> ${INFO} ${C_RED}AMD CPU${NO_FORMAT} detected.\n"
+                        echo -e "${C_WHITE}> ${INFO} ${C_RED}AMD CPU" \
+                                "${NO_FORMAT} detected.\n"
                         cpuBrand="AMD"
                         ;;
                 *)  
-                        echo -e "${C_WHITE}> ${INFO} ${C_YELLOW}Could not detect your CPU vendor. No microcode will be installed.${NO_FORMAT}\n"
+                        echo -e "${C_WHITE}> ${INFO} ${C_YELLOW}Could " \
+                                "not detect your CPU vendor. No microcode will " \
+                                "be installed.${NO_FORMAT}\n"
                         cpuBrand="UNKNOWN"
                         ;;
         esac
