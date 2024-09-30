b7beadb728e7cc70b8f73443b2f91c27a355eaf4
2ELCN0168 <lcn.en.perso@protonmail.com>
2024-09-30 20:54:17 +0200

    Reformatted file

--- functions/f_luks_handling.sh
+++ functions/f_luks_handling.sh
@@ -1,26 +1,39 @@
-# FUNCTION(S)
-# ---
+#
+### File: f_luks_handling.sh
+#
+### Description: 
 # This function creates a LUKS format partition if the user answered "yes" to one of the first questions.
-# EDIT 1 : Modified variables declarations and tests in conditions + replaced by echo.
-# ---
+#
+### Author: 2ELCN0168
+# Last updated: 2024-09-30
+#
+### Dependencies:
+# - cryptsetup.
+#
+### Usage:
+#
+# 1. Encrypt the root partition and replace the variable by its new location.
+#
 
 luks_handling() {
 
-        # FORMATTING DONE
         echo -e "${C_WHITE}> ${INFO} Starting to encrypt your new system...\n"
 
         if cryptsetup luksFormat "${root_part}"; then
-                echo -e "\n${C_WHITE}> ${SUC} ${C_GREEN}Successfully created LUKS partition on ${root_part}.${NO_FORMAT}\n"
+                echo -e "\n${C_WHITE}> ${SUC} ${C_GREEN}Successfully created" \
+                        "LUKS partition on ${root_part}.${NO_FORMAT}\n"
         else
-                echo -e "\n${C_WHITE}> ${ERR} ${C_RED}Error during LUKS partition creation on ${root_part}.${NO_FORMAT}\n"
+                echo -e "\n${C_WHITE}> ${ERR} ${C_RED}Error during LUKS" \
+                        "partition creation on ${root_part}.${NO_FORMAT}\n"
                 exit 1
         fi
 
         echo -e "${C_WHITE}> ${INFO} Opening the new encrypted volume.\n"
 
-        cryptsetup open "${root_part}" root
+        cryptsetup open "${root_part}" "root" || exit 1
 
-        echo -e "\n${C_WHITE}> ${INFO} Replacing ${root_part} by ${C_PINK}/dev/mapper/root.${NO_FORMAT}\n"
+        echo -e "\n${C_WHITE}> ${INFO} Replacing ${root_part} by${C_PINK}" \
+                "/dev/mapper/root.${NO_FORMAT}\n"
 
         root_part="/dev/mapper/root"
 }
