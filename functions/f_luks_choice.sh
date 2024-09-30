930276c68f7a7c0ef5c0d92f6201db7c46a53ae6
2ELCN0168 <lcn.en.perso@protonmail.com>
2024-09-30 20:54:13 +0200

    Reformatted file

--- functions/f_luks_choice.sh
+++ functions/f_luks_choice.sh
@@ -1,35 +1,46 @@
-# FUNCTION(S)
-# ---
+#
+### File: f_luks_choice.sh
+#
+### Description: 
 # This function asks the user if they want to use LUKS to encrypt their filesystem.
-# EDIT 1 : Modified variables declarations and tests in conditions + replaced by echo.
-# ---
+#
+### Author: 2ELCN0168
+# Last updated: 2024-09-30
+#
+### Dependencies:
+# - cryptsetup.
+#
+### Usage:
+#
+# 1. Don't encrypt if the parameter "minimal" is set;
+# 2. Else, ask for LUKS.
+#
 
 luks_choice() {
 
         export wantEncrypted=0
 
-        # FORMATTING DONE
+        [[ "${param_minimal}" -eq 1 ]] && return
+
         while true; do
 
-                if [[ "${param_minimal}" -eq 1 ]]; then
-                        break
-                fi
-
-                echo -e "${C_CYAN}:: ${C_WHITE}Do you want your system to be encrypted with ${B_RED} LUKS ${NO_FORMAT} ? [y/N] -> ${NO_FORMAT}\c"
+                echo -e "${C_CYAN}:: ${C_WHITE}Do you want your system to be" \
+                        "encrypted with ${B_RED} LUKS ${NO_FORMAT} ? [y/N] -> \c"
 
                 local ans_luks=""
                 read ans_luks
                 : "${ans_luks:=N}"
-                # echo ""
 
                 case "${ans_luks}" in
-                        "y"|"Y")
-                                echo -e "${C_WHITE}> ${INFO} ${C_GREEN}cryptsetup${NO_FORMAT} will be installed.\n"
+                        [yY])
+                                echo -e "${C_WHITE}> ${INFO} ${C_GREEN}cryptsetup" \
+                                        "${NO_FORMAT} will be installed.\n"
                                 wantEncrypted=1
                                 break
                                 ;;
-                        "n"|"N")
-                                echo -e "${C_WHITE}> ${INFO} ${C_RED}cryptsetup${NO_FORMAT} won't be installed.\n"
+                        [nN])
+                                echo -e "${C_WHITE}> ${INFO} ${C_RED}cryptsetup" \
+                                        "${NO_FORMAT} won't be installed.\n"
                                 wantEncrypted=0
                                 break
                                 ;;
