141d5c43498eac3ad0501be6ad5fb05e65554629
2ELCN0168 <lcn.en.perso@protonmail.com>
2024-10-18 20:53:11 +0200

    Added a variable for sudo prompt

--- post_install/files/profile.d/env.sh
+++ post_install/files/profile.d/env.sh
@@ -5,3 +5,4 @@ export GREP_COLORS="mt=01;91"
 export EDITOR="vim"
 export VISUAL="vim"
 export GIT_EDITOR="vim"
+export SUDO_PROMPT=$(printf "\n\033[96;1m/,\\ \033[0;93mEnter your password: \033[0m")
