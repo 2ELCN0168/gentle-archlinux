f81568295b2b3c42abb1809194ebf5d889c07297
2ELCN0168 <lcn.en.perso@protonmail.com>
2024-10-20 21:43:50 +0200

    Changed orded

--- post_install/functions/f_set_shellrc.sh
+++ post_install/functions/f_set_shellrc.sh
@@ -1,10 +1,5 @@
 set_term_env() {
 
-        [[ "${nKorea}" -eq 1 ]] && \
-        printf "\nalias fastfetch='fastfetch --logo redstaros'\n" \
-        1>> "/etc/skel/.bashrc" \
-        1>> "/etc/skel/.zshrc"
-
         cp "/post_install/files/.bashrc_template" "/etc/skel/.bashrc"
         cp "/post_install/files/.zshrc_template" "/etc/skel/.zshrc"
 
@@ -12,5 +7,12 @@ set_term_env() {
         cp "/etc/skel/.bash_profile" "/root"
         cp "/etc/skel/.zshrc" "/root"
 
-        cp "/post_install/files/profile.d/"* "/etc/profile.d/"
+        [[ ! -e "/etc/shell_config.d" ]] && mkdir "/etc/shell_conf.d"
+
+        cp "/post_install/files/shell_conf.d/"* "/etc/shell_conf.d/"
+
+        [[ "${nKorea}" -eq 1 ]] && \
+        printf "\nalias fastfetch='fastfetch --logo redstaros'\n" \
+        1>> "/etc/skel/.bashrc" \
+        1>> "/etc/skel/.zshrc"
 }
