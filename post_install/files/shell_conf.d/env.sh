43aa4287f91e03b2fc251b675808ec6c346936ca
2ELCN0168 <lcn.en.perso@protonmail.com>
2024-10-23 19:17:38 +0200

    Modified SUDO_PROMPT

--- post_install/files/shell_conf.d/env.sh
+++ post_install/files/shell_conf.d/env.sh
@@ -5,4 +5,4 @@ export GREP_COLORS="mt=01;91"
 export EDITOR="vim"
 export VISUAL="vim"
 export GIT_EDITOR="vim"
-export SUDO_PROMPT=$(printf "\n\033[92;1m:: \033[0;90mEnter your password: \033[0m")
+export SUDO_PROMPT=$(printf "\n\033[92;1m[>] \033[0;90mEnter your password \033[92m> \033[0m")
