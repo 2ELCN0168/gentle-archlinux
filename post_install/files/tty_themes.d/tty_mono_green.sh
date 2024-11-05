9376931d2cf41df6bb281cc91e46aabd0c3ed4f0
Enzo <eluciani@cpzardaigoro01o.home.arpa>
2024-11-05 17:11:12 +0100

    Corrected theme

--- post_install/files/tty_themes.d/tty_mono_green.sh
+++ post_install/files/tty_themes.d/tty_mono_green.sh
@@ -2,22 +2,22 @@ __tty_theme() {
 
         [ "$TERM" = 'linux' ] || return # Only run in a TTY
 
-	printf "\e]P0031f1a"       # black          #031f1a
-	printf "\e]P183cdb1"       # red            #83cdb1
-	printf "\e]P283cdb1"       # green          #83cdb1
-	printf "\e]P383cdb1"       # brown          #83cdb1
-	printf "\e]P483cdb1"       # blue           #83cdb1
-	printf "\e]P583cdb1"       # magenta        #83cdb1
-	printf "\e]P683cdb1"       # cyan           #83cdb1
-	printf "\e]P783cdb1"       # white          #83cdb1
-	printf "\e]P8031f1a"       # bright-black   #031f1a
-	printf "\e]P983cdb1"       # bright-red     #83cdb1
-	printf "\e]PA83cdb1"       # bright-green   #83cdb1
-	printf "\e]PB83cdb1"       # bright-brown   #83cdb1
-	printf "\e]PC83cdb1"       # bright-blue    #83cdb1
-	printf "\e]PD83cdb1"       # bright-magenta #83cdb1
-	printf "\e]PE83cdb1"       # bright-cyan    #83cdb1
-	printf "\e]PF83cdb1"       # bright-white   #83cdb1
+	printf "\e]P0041412"       # black          #041412
+	printf "\e]P126de9a"       # red            #26de9a
+	printf "\e]P226de9a"       # green          #26de9a
+	printf "\e]P326de9a"       # brown          #26de9a
+	printf "\e]P426de9a"       # blue           #26de9a
+	printf "\e]P526de9a"       # magenta        #26de9a
+	printf "\e]P626de9a"       # cyan           #26de9a
+	printf "\e]P726de9a"       # white          #26de9a
+	printf "\e]P8041412"       # bright-black   #041412
+	printf "\e]P926de9a"       # bright-red     #26de9a
+	printf "\e]PA26de9a"       # bright-green   #26de9a
+	printf "\e]PB26de9a"       # bright-brown   #26de9a
+	printf "\e]PC26de9a"       # bright-blue    #26de9a
+	printf "\e]PD26de9a"       # bright-magenta #26de9a
+	printf "\e]PE26de9a"       # bright-cyan    #26de9a
+	printf "\e]PF26de9a"       # bright-white   #26de9a
 
         clear
 }
