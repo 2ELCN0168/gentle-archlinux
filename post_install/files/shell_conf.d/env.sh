e4d9f68be463ab06ea5497da03f33290e356024b
Enzo <eluciani@cpzardaigoro01o.home.arpa>
2024-11-06 09:37:44 +0100

    Corrected SUDO_PROMPT

--- post_install/files/shell_conf.d/env.sh
+++ post_install/files/shell_conf.d/env.sh
@@ -5,4 +5,4 @@ export GREP_COLORS="mt=01;91"
 export EDITOR="vim"
 export VISUAL="vim"
 export GIT_EDITOR="vim"
-export SUDO_PROMPT=$(printf "\n\033[92;1m[>] \033[97;0mEnter your password \033[92m> \033[0m")
+export SUDO_PROMPT=$(printf "\n\033[92;1m[?] \033[97mEnter your password \033[92m\n\033[94m[>] \033[0m")
