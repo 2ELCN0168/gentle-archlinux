6b07100b48c47aa0f7a53002b92c35e0fca0dcfa
Enzo <eluciani@cpzardaigoro01o.home.arpa>
2024-11-07 18:55:08 +0100

    Corrected infinite loop bug

--- functions/f_network_manager_choice.sh
+++ functions/f_network_manager_choice.sh
@@ -31,20 +31,18 @@ net_manager() {
                 read -r ans_net_manager
                 : "${ans_net_manager:=0}"
 
-                [[ "${ans_net_manager}" =~ [yYnN] ]] && break
+                [[ "${ans_net_manager}" =~ [012] ]] && break
         done
 
         if [[ "${ans_net_manager}" -eq 0 ]]; then
                 printf "${C_W}> ${INFO} You chose "
                 printf "${C_G}systemd-networkd${N_F}.\n\n"
                 net_manager="systemd-networkd"
-                break
         elif [[ "${ans_net_manager}" -eq 1 ]]; then
                 printf "${C_W}> ${INFO} You chose "
                 printf "${C_C}NetworkManager${N_F}.\n\n"
                 net_manager="networkmanager" 
-                break
-        elif [[ "${ans_net_manager}" -eq 9 ]]; then
+        elif [[ "${ans_net_manager}" -eq 2 ]]; then
                 return
         else
                 invalid_answer
@@ -57,7 +55,7 @@ net_menu() {
 
         printf "${C_W}[0] - ${C_G}systemd-networkd${N_F} [default]\n"
         printf "${C_W}[1] - ${C_C}NetworkManager${N_F}\n"
-        printf "${C_Y}[9] - Skip ->${N_F}\n\n"
+        printf "${C_Y}[2] - Skip ->${N_F}\n\n"
 
         printf "────────────────────────────────────────\n\n"
 
