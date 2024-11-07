5e7b6308cd764a30a7b6bfec186e38c63e15b1c4
Enzo <eluciani@cpzardaigoro01o.home.arpa>
2024-11-07 20:05:47 +0100

    Rewritten logic

--- functions/lvm_luks/f_lvm_luks_deletion.sh
+++ functions/lvm_luks/f_lvm_luks_deletion.sh
@@ -6,7 +6,7 @@
 # pursuie the installation.
 #
 ### Author: 2ELCN0168
-# Last updated: 2024-10-04
+# Last updated: 2024-10-07
 #
 ### Dependencies:
 # - lvm2;
@@ -39,9 +39,12 @@ lvm_luks_try() {
                 printf "${C_C}LVM ${C_Y}nor ${C_C}LUKS${C_Y}, continue..."
                 printf "${N_F}\n\n"
         fi
-        [[ "${result}" -eq 1 ]] && lvm_deletion
-        [[ "${result}" -eq 2 ]] && luks_deletion
-        [[ "${result}" -eq 3 ]] && luks_deletion && lvm_deletion
+
+        case "${result}" in 
+                1) lvm_deletion ;;
+                2) luks_deletion ;;
+                3) luks_deletion && lvm_deletion ;;
+        esac
 }
 
 lvm_deletion() {
@@ -55,27 +58,58 @@ lvm_deletion() {
                 : "${ans_wipe_lvm:=N}"
                 printf "\n"
 
-                local vg_name=($(vgs --noheadings | awk '{ print $1 }'))
-                if [[ "${ans_wipe_lvm}" =~ [yY] ]]; then
-                        for i in "${vg_name}"; do
-                                lvremove -f -y "/dev/mapper/${vg_name}-*" \
-                                1> "/dev/null" 2>&1
-                                vgremove -f -y "${vg_name}" \
-                                1> "/dev/null" 2>&1
-                        done
-                        for i in $(pvs | tail -n +2 | awk '{ print $1 }'); do
-                                pvremove -f -y "${i}" 1> "/dev/null" 2>&1
-                        done
-                        printf "${C_W}> ${SUC} ${C_P}LVM deleted.${N_F}\n\n"
-                        return
-                elif [[ "${ans_wipe_lvm}" =~ [nN] ]]; then 
-                        printf "${C_W}> ${WARN} ${C_P}No LVM will be "
-                        printf "deleted.${N_F}\n\n"
-                        return
-                else
-                        invalid_answer
+                [[ "${ans_wipe_lvm}" =~ [yYnN] ]] && break || invalid_answer
+        done
+
+        # local vg_name=($(vgs --noheadings | awk '{ print $1 }'))
+        # if [[ "${ans_wipe_lvm}" =~ [yY] ]]; then
+        #         for i in "${vg_name}"; do
+        #                 lvremove -f -y "/dev/mapper/${vg_name}-*" \
+        #                 1> "/dev/null" 2>&1
+        #                 vgremove -f -y "${vg_name}" \
+        #                 1> "/dev/null" 2>&1
+        #         done
+        #         for i in $(pvs | tail -n +2 | awk '{ print $1 }'); do
+        #                 pvremove -f -y "${i}" 1> "/dev/null" 2>&1
+        #         done
+        #         printf "${C_W}> ${SUC} ${C_P}LVM deleted.${N_F}\n\n"
+        #         return
+        # elif [[ "${ans_wipe_lvm}" =~ [nN] ]]; then 
+        #         printf "${C_W}> ${WARN} ${C_P}No LVM will be "
+        #         printf "deleted.${N_F}\n\n"
+        #         return
+        # fi
+        #
+
+        if [[ "${ans_wipe_lvm}" =~ [nN] ]]; then
+                printf "${C_W}> ${WARN} ${C_P}No LVM will be "
+                printf "deleted.${N_F}\n\n"
+                return
+        fi
+
+        local err_counter=0
+
+        for i in "$(vgs --noheadings | awk '{ print $1 }')"; do
+                if ! lvremove -f -y "/dev/${i}"* 1> "/dev/null" 2>&1; then
+                        ((err_counter++))
+                fi
+                if ! vgremove -f -y "${i}" 1> "/dev/null" 2>&1; then
+                        ((err_counter++))
                 fi
         done
+
+        for i in $(pvs --noheadings | awk '{ print $1 }'); do
+                if ! pvremove -f -y "${i}" 1> "/dev/null" 2>&1; then
+                        ((err_counter++))
+                fi
+        done
+
+        if [[ "${err_counter}" -eq 0 ]]; then
+                printf "${C_W}> ${SUC} ${C_P}LVM deleted.${N_F}\n\n"
+        else
+                printf "${C_W}> ${ERR} ${C_R}LVM couldn't be "
+                printf "deleted.${N_F}\n\n"
+        fi
 }
 
 luks_deletion() {
@@ -87,19 +121,22 @@ luks_deletion() {
                 local ans_close_luks=""
                 read ans_close_luks
                 : "${ans_close_luks:=Y}"
-                echo ""
+                printf "\n"
 
-                if [[ "${ans_close_luks}" =~ [yY] ]]; then
-                        cryptsetup close root 1> "/dev/null" 2>&1
+                [[ "${ans_close_luks}" =~ [yYnN] ]] && break || invalid_answer
+        done
+
+        if [[ "${ans_close_luks}" =~ [yY] ]]; then
+                if cryptsetup close root 1> "/dev/null" 2>&1; then
                         printf "${C_W}> ${SUC} ${C_P}LUKS partition "
                         printf "closed.${N_F}\n\n"
-                        return
-                elif [[ "${ans_close_luks}" =~ [nN] ]]; then 
-                        printf "${C_W}> ${WARN} ${C_P}No LUKS partition will "
-                        printf "be closed.${N_F}\n\n"
-                        return
                 else
-                        invalid_answer
+                        printf "${C_W}> ${ERR} ${C_P}LUKS partition "
+                        printf "could not be closed.${N_F}\n\n"
+                        exit 1
                 fi
-        done
+        elif [[ "${ans_close_luks}" =~ [nN] ]]; then 
+                printf "${C_W}> ${WARN} ${C_P}No LUKS partition will "
+                printf "be closed.${N_F}\n\n"
+        fi
 }
