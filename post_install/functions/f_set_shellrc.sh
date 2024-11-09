a9a3897013c67bf66358f9d1dae6b098a5aabd91
Enzo <eluciani@cpzardaigoro01o.home.arpa>
2024-11-09 20:05:10 +0100

    Replaced zsh-syntax-highlighting by zsh-fast-syntax-highlighting

--- post_install/functions/f_set_shellrc.sh
+++ post_install/functions/f_set_shellrc.sh
@@ -27,4 +27,19 @@ set_term_env() {
         printf "\nalias fastfetch='fastfetch --logo redstaros'\n" \
         1>> "/etc/skel/.bashrc" \
         1>> "/etc/skel/.zshrc"
+
+
+
+        printf "${C_W}> ${INFO} Cloning ${C_G}zsh-fast-syntax-highlighting "
+        printf "${N_F} into ${C_P}/usr/share/zsh/plugins${N_F}...\n"
+
+        if git clone \
+        "https://github.com/zdharma-continuum/fast-syntax-highlighting" \
+        "/usr/share/zsh/plugins/zsh-fast-syntax-highlighting/"; then
+                printf "${C_W}> ${SUC} Cloned ${C_G}zsh-fast-syntax-"
+                printf "highlighting repository."
+        else
+                printf "${C_W}> ${ERR} Could not clone ${C_G}zsh-fast-syntax-"
+                printf "highlighting repository."
+        fi
 }
