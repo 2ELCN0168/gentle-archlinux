b2ef761ae2db8add2e7e49268a53cebf5dc303f0
Enzo <eluciani@cpzardaigoro01o.home.arpa>
2024-11-21 20:45:30 +0100

    Corrected typo

--- post_install/functions/f_set_shellrc.sh
+++ post_install/functions/f_set_shellrc.sh
@@ -34,15 +34,16 @@ set_term_env() {
 
 
         printf "${C_W}> ${INFO} Cloning ${C_G}zsh-fast-syntax-highlighting "
-        printf "${N_F} into ${C_P}/usr/share/zsh/plugins${N_F}...\n"
+        printf "${N_F}into ${C_P}/usr/share/zsh/plugins${N_F}...\n"
 
         if git clone \
         "https://github.com/zdharma-continuum/fast-syntax-highlighting" \
-        "/usr/share/zsh/plugins/zsh-fast-syntax-highlighting/"; then
+        "/usr/share/zsh/plugins/zsh-fast-syntax-highlighting/" \
+        1> "/dev/null" 2>&1; then
                 printf "${C_W}> ${SUC} Cloned ${C_G}zsh-fast-syntax-"
-                printf "highlighting repository."
+                printf "highlighting repository.\n"
         else
                 printf "${C_W}> ${ERR} Could not clone ${C_G}zsh-fast-syntax-"
-                printf "highlighting repository."
+                printf "highlighting repository.\n"
         fi
 }
